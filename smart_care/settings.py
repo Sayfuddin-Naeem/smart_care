ab47fe38ed51eae4168d89fd1ee892f478198494
Sayfuddin-Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2024-08-27 22:37:30 +0600

    cros origin resolved

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -31,10 +31,10 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://mamar-library-iah0.onrender.com',
-#     'https://*.127.0.0.1'
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://mamar-smart-care.onrender.com',
+    'https://*.127.0.0.1'
+]
 
 
 # Application definition
